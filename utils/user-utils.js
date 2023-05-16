24b7bffc400cc20ad1e7239f16e9131353de44ff
Victor Villalba <villalbabesada@gmail.com>
2023-05-16 18:34:44 +0200

    crud donde

--- utils/user-utils.js
+++ utils/user-utils.js
@@ -9,4 +9,12 @@ function renderizeButtom(user, id) {
     return { isAdmin: user?.role === "Admin", isUser: user?._id == id }
 }
 
-module.exports = { noAvatar, renderizeButtom }
+function logged(currentUser, id) {
+    return currentUser && currentUser._id === id;
+}
+function logOut(currentUser, id) {
+    return currentUser && currentUser._id === id;
+}
+
+
+module.exports = { noAvatar, renderizeButtom, logged, logOut }
