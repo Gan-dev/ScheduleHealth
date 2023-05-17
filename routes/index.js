1f1bae81b7bcb1506e2bb7aa9b0362a4369ae003
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-05-17 13:12:30 +0200

    hola

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+module.exports = app => {
+
+    app.use((req, res, next) => {
+        res.locals.currentUser = req.session.currentUser;
+        next();
+    });
+    const indexRoutes = require("./index.routes");
+    app.use("/", indexRoutes);
+    const authRoutes = require("./auth.routes")
+    app.use("/", authRoutes)
+    const newsRoutes = require("./news.routes")
+    app.use("/", newsRoutes)
+    const calendarRoutes = require("./calendar.routes")
+    app.use("/", calendarRoutes)
+    const userRoutes = require("./user.routes")
+    app.use("/", userRoutes)
+    const eventsRoutes = require("./events.routes")
+    app.use("/", eventsRoutes)
+
+}
